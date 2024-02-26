65343941317b610f9fb61212e38e1d1d1120994d
wonsunahn <wonsunahn@gmail.com>
2024-02-26 09:07:15 -0500

    Added branch misprediction performance counters.

--- resources/branch_experiments/branch.c
+++ resources/branch_experiments/branch.c
@@ -4,17 +4,13 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(int args, char **argv)
-{
-    if (args != 3) {
-        printf("Usage: branch <array size> <sort|nosort>\n");
-        return 0;
-    }
+size_t arraySize;
+bool sorted;
+std::vector<int> data;
 
+void *create(void *unused) {
     // generate data
-    const size_t arraySize = atoi(argv[1]);
-    bool sorted = strcmp(argv[2], "sort") == 0;
-    std::vector<int> data(arraySize);
+    data.resize(arraySize);
 
     for (unsigned c = 0; c < arraySize; ++c)
         data[c] = std::rand() % 256;
@@ -23,9 +19,10 @@ int main(int args, char **argv)
     if (sorted) {
         std::sort(data.begin(), data.end());
     }
+}
 
+void *run(void *unused) {
     long long sum = 0;
-
     for (unsigned i = 0; i < 10000; ++i)
     {
         for (unsigned c = 0; c < arraySize; ++c)
@@ -35,5 +32,23 @@ int main(int args, char **argv)
         }
     }
     printf("sum = %lld\n", sum);
+}
+
+int main(int args, char **argv)
+{
+    if (args != 3) {
+        printf("Usage: branch <array size> <sort|nosort>\n");
+        return 0;
+    }
+    arraySize = atoi(argv[1]);
+    sorted = strcmp(argv[2], "sort") == 0;
+
+    pthread_t tCreate, tRun;
+    pthread_create(&tCreate, NULL, create, NULL);
+    pthread_join(tCreate, NULL);
+    pthread_create(&tRun, NULL, run, NULL);
+    pthread_join(tRun, NULL);
+
+    return 0;
 }
 
